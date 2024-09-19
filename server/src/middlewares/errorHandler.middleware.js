7398c79e2e8f076ff03967bfdfd8bfbb3bdd91d2
Pritam <pritambose040@gmail.com>
2024-09-19 15:51:47 +0530

    minor update

--- server/src/middlewares/errorHandler.middleware.js
+++ server/src/middlewares/errorHandler.middleware.js
@@ -13,6 +13,8 @@ const errorHandler = (err, req, res, next) => {
     res.status(500).json({
       success: false,
       message: "Internal Server Error",
+      errors: [],
+      stack: err.stack,
     });
   }
 };
